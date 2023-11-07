c5b7af1e3ad2402406a297fcd118e5c54657aa6e
Georgi Toni Georgiev <92455881+g-t-georgiev@users.noreply.github.com>
2023-11-07 16:17:57 +0200

    Removed custom cursor implementation logic.

--- script.js
+++ script.js
@@ -7,56 +7,6 @@ const rotateHandleElem = document.querySelector('[data-box-rotate-handle]');
 /** @type HTMLElement */
 const resizeHandleElem = document.querySelector('[data-box-resize-handle]');
 
-const cursors = {
-    types: {
-        move: document.querySelector('[data-cursor="move"]'),
-        resize: document.querySelector('[data-cursor="resize"]'),
-        rotate: document.querySelector('[data-cursor="rotate"]'),
-    },
-    /**
-     * @param {SVGAElement} cursor 
-     * @param {PointerEvent} event 
-     */
-    attach(cursor, event) {
-        if (!cursor) return;
-
-        if (cursors.current) {
-            if (cursors.current.classList.contains('active')) return;
-            cursors.current.replaceWith(cursor);
-            cursors.current.classList.add('in-scope');
-            return;
-        }
-
-        document.body.append(cursor);
-        cursors.current = cursor;
-        cursors.current.classList.add('in-scope');
-    },
-    /**
-     * Detach cursor
-     * @param {PointerEvent} event
-     */
-    detach(event) {
-        if (!cursors.current) return;
-        
-        cursors.current.classList.remove('in-scope');
-        if (cursors.current.classList.contains('active')) return;
-        
-        cursors.current.remove();
-        cursors.current = null;
-    },
-    /**
-     * Move cursor.
-     * @param {PointerEvent} event 
-     */
-    move(event) {
-        if (cursors.current && event) {
-            cursors.current.style.setProperty('--x', event.clientX);
-            cursors.current.style.setProperty('--y', event.clientY);
-        }
-    },
-    current: null
-};
-
 const boxRect = {
     width: 150,
     height: 150,
@@ -69,10 +19,6 @@ let viewportHeight = window.innerHeight;
 let isDragging = false;
 let isResizing = false;
 let isRotating = false;
-
-cursors.types.move.remove();
-cursors.types.resize.remove();
-cursors.types.rotate.remove();
 initialize();
 
 function setupActions() {
@@ -91,7 +37,6 @@ function initRotatingActions() {
     rotateHandleElem.addEventListener('mousedown', function (event) {
         if (isDragging || isResizing) return;
         event.preventDefault();
-        toggleCursor(cursors.types.rotate, true);
         center.x = boxRect.x + (boxRect.width / 2);
         center.y = boxRect.y + (boxRect.height / 2);
         let x = event.clientX - center.x;
@@ -104,8 +49,6 @@ function initRotatingActions() {
         event.preventDefault();
         totalAngle += rotationAngle;
         isRotating = false;
-        toggleCursor(cursors.types.rotate, false);
-        if (!cursors.current.classList.contains('in-scope')) cursors.detach();
     });
     document.addEventListener('mousemove', function (event) {
         if (!isRotating || isResizing || isDragging) return;
@@ -125,7 +68,6 @@ function initResizingActions() {
     resizeHandleElem.addEventListener('mousedown', function (event) {
         if (isDragging || isRotating) return;
         event.preventDefault();
-        toggleCursor(cursors.types.resize, true);
         x = event.clientX - x;
         y = event.clientY - y;
         isResizing = true;
@@ -136,8 +78,6 @@ function initResizingActions() {
         x = Math.min(Math.max(event.clientX - x, 0), viewportWidth - boxRect.x - 8.5);
         y = Math.min(Math.max(event.clientY - y, 0), viewportHeight - boxRect.y - 8.5);
         isResizing = false;
-        toggleCursor(cursors.types.resize, false);
-        if (!cursors.current.classList.contains('in-scope')) cursors.detach();
     });
     document.addEventListener('mousemove', function (event) {
         if (!isResizing || isDragging || isRotating) return;
@@ -157,7 +97,6 @@ function initDraggingActions() {
     boxElem.addEventListener('mousedown', function (event) {
         if (isResizing || isRotating) return;
         event.preventDefault();
-        toggleCursor(cursors.types.move, true);
         x = event.clientX - x;
         y = event.clientY - y;
         isDragging = true;
@@ -168,8 +107,6 @@ function initDraggingActions() {
         x = Math.min(Math.max(event.clientX - x, 0), viewportWidth - boxRect.width - 8.5);
         y = Math.min(Math.max(event.clientY - y, 25), viewportHeight - boxRect.height - 8.5);
         isDragging = false;
-        toggleCursor(cursors.types.move, false);
-        if (!cursors.current.classList.contains('in-scope')) cursors.detach();
     });
     document.addEventListener('mousemove', function (event) {
         if (!isDragging || isResizing || isRotating) return;
@@ -180,30 +117,6 @@ function initDraggingActions() {
         boxElem.style.setProperty('--x', boxRect.x);
         boxElem.style.setProperty('--y', boxRect.y);
     });
-}
-
-function setupCursors() {
-    document.addEventListener('pointermove', cursors.move);
-    boxElem.addEventListener('pointerover', function (event) {
-        // console.log(event.target);
-        if (event.target == resizeHandleElem) {
-            cursors.attach.call(this, cursors.types.resize);
-            return;
-        }
-
-        if (event.target == rotateHandleElem) {
-            cursors.attach.call(this, cursors.types.rotate);
-            return;
-        }
-
-        cursors.attach.call(this, cursors.types.move);
-    });
-    boxElem.addEventListener('pointerout', cursors.detach);
-}
-
-function toggleCursor(cursor, flag = false) {
-    if (!cursor) return;
-    cursor.classList.toggle('active', flag);
 }
 
 function setupBox() {
@@ -233,7 +146,6 @@ function initialize() {
     viewportWidth = window.innerWidth;
     viewportHeight = window.innerHeight;
 
-    setupCursors();
     setupBox();
     setupActions();
 
